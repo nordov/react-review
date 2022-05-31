ec3e1b426dd505d92848a560f7a95e60189e256d
Nordov <nordov@gmail.com>
2022-05-30 19:14:23 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+function Profile({ userData }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{userData.name}</h1>
+      <p>{userData.location}</p>
+      <img src={userData.avatar_url} alt={userData.login} width={160} />
     </div>
   );
+}
+
+function App({ username }) {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!username) return;
+    setLoading(true);
+    setTimeout(() => {
+      fetch(`https://api.github.com/users/${username}`)
+        .then(response => response.json())
+        .then(setUserData)
+        .then(setLoading(false))
+        .catch(setError);
+    }, 3000)
+  }, [username]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if (!userData) return null;
+
+  if (userData)
+    return <Profile userData={userData} />;
+
+  return <div>No data found for {username}</div>;
 }
 
 export default App;
